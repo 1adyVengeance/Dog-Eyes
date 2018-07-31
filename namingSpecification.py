a467c55f2bad923e7562aade9f101d91d24821f7
L.V <w3300306@126.com>
2018-07-31 17:37:20 +0800

    'initialization'

---
+++ namingSpecification.py
@@ -0,0 +1,30 @@
+def function0(a, b, c):
+    """
+    explanation
+    :param a: =======
+    :param b: =======
+    :param c: =========
+    :return: ========
+    """
+    pass
+
+
+def my_function(a, b):
+    """
+    explanation
+    :param a: ==========
+    :param b: ==========
+    :return: ===========
+    """
+
+
+class Class0(object):
+    pass
+
+
+class ClassName(object):
+    pass
+
+
+variable = None
+new_variable = None
